7148e98e60ee636d8904a5db72957cdda708dcc8
Steve Mustanski <steve.mustanski@gmail.com>
2018-09-03 11:51:45 -0400

    added try on json parse of badge app

--- node-basics/badges/app.js
+++ node-basics/badges/app.js
@@ -6,36 +6,41 @@ const https = require('https');
 
 function getProfile(username) {
   try {
-  // function to print message to console
-  function printMessage(username, badgeCount, points) {
-    const message = `${username} has ${badgeCount} total badge(s) and ${points} JavaScript points`;
-    console.log(message);
+    // function to print message to console
+    function printMessage(username, badgeCount, points) {
+      const message = `${username} has ${badgeCount} total badge(s) and ${points} JavaScript points`;
+      console.log(message);
+    }
+    // connect to the API URL https://teamtreehouse.com/username.json
+    const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+      let body = '';
+
+      // read the data
+      response.on('data', data => {
+        body += data.toString();
+      });
+
+      response.on('end', () => {
+        try {
+          // Parse the data
+          const profile = JSON.parse(body);
+          // Printout the data
+          printMessage(username, profile.badges.length, profile.points.JavaScript);
+
+        } catch (error) {
+          console.error(error.message);
+        }
+      });
+
+    });
+
+    // handle any errors
+    request.on('error', error => {
+      console.error(`Problem with request: ${error.message}`);
+    });
+  } catch (error) {
+    console.error(`There was an error: ${error.message}`);
   }
-  // connect to the API URL https://teamtreehouse.com/username.json
-  const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-    let body = '';
-
-    // read the data
-    response.on('data', data => {
-      body += data.toString();
-    });
-
-    response.on('end', () => {
-      // Parse the data
-      const profile = JSON.parse(body);
-      // Printout the data
-      printMessage(username, profile.badges.length, profile.points.JavaScript);
-    });
-
-  });
-
-  // handle any errors
-  request.on('error', error => {
-    console.error(`Problem with request: ${error.message}`);
-  });
-} catch (error) {
-  console.error(`There was an error: ${error.message}`);
-}
 }
 // gets the arguments from the command line as 3 element in the process.argv array
 // command would be node app.js followed by list of users
