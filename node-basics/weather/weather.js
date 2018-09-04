311136fdf1edda102a03670e8bf622f36f41627b
Steve Mustanski <steve.mustanski@gmail.com>
2018-09-04 16:46:20 -0400

    added error handling to weather application

--- node-basics/weather/weather.js
+++ node-basics/weather/weather.js
@@ -5,26 +5,42 @@ const http = require('http');
 const apiKey = 'aa28338ba3c9e427';
 const feature = 'conditions';
 
-function get(query){
-// get the data for a zipcode
-   const request = http.get(`http://api.wunderground.com/api/${apiKey}/${feature}/q/${query}.json`, response => {
-    let body = '';
+function printError(error) {
+  console.error(error.message);
+}
 
-    response.on('data', data => {
-      body += data.toString();
+function get(query) {
+  try {
+    // get the data for a zipcode
+    const request = http.get(`http://api.wunderground.com/api/${apiKey}/${feature}/q/${query}.json`, response => {
+      if (response.statusCode === 200) {
+        let body = '';
+
+        response.on('data', data => {
+          body += data.toString();
+        });
+
+        // log out the response 
+        response.on('end', () => {
+          const forecast = JSON.parse(body);
+          const location = (forecast.current_observation.display_location.full);
+          const temp_f = (forecast.current_observation.temp_f);
+          console.log(`The current temperature for ${location} is ${temp_f} degrees.`);
+        });
+
+      } else {
+        const message = `There was an error getting weather data for ${query}. (${http.STATUS_CODES[response.statusCode]})`;
+        const statusCodeError = new Error(message);
+        printError(statusCodeError);
+      }
+
     });
 
-    // log out the response 
-    response.on('end', () =>{
-      const forecast = JSON.parse(body);
-      const location = (forecast.current_observation.display_location.full);
-      const temp_f = (forecast.current_observation.temp_f);
-      console.log(`The current temperature for ${location} is ${temp_f} degrees.`);
-    });
-
-  
-   });
+    request.on('error', printError);
+  } catch (error) {
+    printError(error);
   }
+}
 
 module.exports.get = get;
 
