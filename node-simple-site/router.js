28a4f739066904649c15c91dd5913de72e2934ac
Steve Mustanski <steve.mustanski@gmail.com>
2018-09-08 17:06:58 -0400

    added processing of user pages to user route for profile page

--- node-simple-site/router.js
+++ node-simple-site/router.js
@@ -1,3 +1,6 @@
+const Profile = require('./profile.js');
+
+
 // Handle HTTP route GET / and POST /
 function home(request, response) {
   if (request.url === '/') {
@@ -11,17 +14,33 @@ function home(request, response) {
 }
 
 // Handle HTTP route GET /:username
-function user (request, response) {
+function user(request, response) {
   let username = request.url.replace('/', '');
-  if (username.length > 0 )  {
+  if (username.length > 0) {
     response.setHeader('Content-Type', 'text/plain');
     response.write('Header\n');
-    response.write(username + '\n');
-    response.end('Footer\n');
+    // get JSON from treehouse
+    let studentProfile = new Profile(username);
+    //  on end, show profile
+    studentProfile.on('end', (profileJSON) => {
+      let values = {
+        avatarURL: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      }
+      response.write(`${values.username} has ${values.badges} badges\n`);
+      response.end('Footer\n');
+    });
+    //  on error, show error
+    studentProfile.on('error', (error) => {
+      response.end('Footer\n');
+    });
 
-//  if the url == / anything, get JSON from treehouse
-//  on end, show profile
-//  on error, show error
+
+
+
+
   }
 }
 
