2e894d0dfa487e303f71053a8b10391679b5bb5f
Steve Mustanski <steve.mustanski@gmail.com>
2018-09-11 23:51:31 -0400

    populate temlates with user data for profile

--- node-simple-site/renderer.js
+++ node-simple-site/renderer.js
@@ -1,11 +1,22 @@
 const fs = require('fs');
 
+function mergeValues(values, content) {
+  // cycle over keys
+  for (var key in values) {
+    // replace keys with values
+    content = content.replace('{{' + key + '}}', values[key]);
+  }
+
+  // return merged content
+  return content;
+
+}
 
 function view(templateName, values, response) {
   //read from the template files
-  let fileContents = fs.readFileSync(`./views/${templateName}.html`)
+  let fileContents = fs.readFileSync(`./views/${templateName}.html`, {encoding: 'utf8'});
   // insert value into content
-
+  fileContents = mergeValues(values, fileContents);
   // write out to the response
   response.write(fileContents);
 }
