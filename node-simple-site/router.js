8547683e177bf72ab0abf2320d2a4c0987514dcd
Steve Mustanski <steve.mustanski@gmail.com>
2018-09-14 16:54:26 -0400

    added redirect to /username on profile page

--- node-simple-site/router.js
+++ node-simple-site/router.js
@@ -1,25 +1,40 @@
 const Profile = require('./profile.js');
 const renderer = require('./renderer');
+const queryString = require('querystring');
+
 
 
 // Handle HTTP route GET / and POST /
 function home(request, response) {
-  if (request.url === '/') {
-    response.setHeader('Content-Type', 'text/html');
-    renderer.view('header', {}, response);
-    renderer.view('Search', {}, response);
-    renderer.view('Footer', {}, response);
-    response.end();
-  }
   //  if the url == / && GET - show search
-  //  if the url == / && POST - redirect to /:username
+  if (request.url === '/') {
+    if (request.method.toLowerCase() === "get") {
+      response.writeHead(200, { 'Content-Type': 'text/html' });
+      renderer.view('header', {}, response);
+      renderer.view('Search', {}, response);
+      renderer.view('Footer', {}, response);
+      response.end();
+    } else {
+      //  if the url == / && POST - redirect to /:username
+      // get the post data from body
+      request.on('data', (postBody) => {
+        // extract the username
+        let query = queryString.parse(postBody.toString());
+        // then redirect to /:username
+        response.writeHead(303, {"Location": "/" + query.username});
+        response.end();
+      })
+    }
+  }
+
+
 }
 
 // Handle HTTP route GET /:username
 function user(request, response) {
   let username = request.url.replace('/', '');
   if (username.length > 0) {
-    response.setHeader('Content-Type', 'text/html');
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     renderer.view('header', {}, response);
     // get JSON from treehouse
     let studentProfile = new Profile(username);
@@ -37,7 +52,7 @@ function user(request, response) {
     });
     //  on error, show error
     studentProfile.on('error', (error) => {
-      renderer.view('error', {errorMessage: error.message}, response);
+      renderer.view('error', { errorMessage: error.message }, response);
       renderer.view('Search', {}, response);
       renderer.view('Footer', {}, response);
       response.end();
