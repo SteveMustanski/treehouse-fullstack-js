3330a6443052f551b588d12e77f58523fa7daf13
Steve Mustanski <steve.mustanski@gmail.com>
2018-09-20 15:17:16 -0400

    added gulp-concat to combine all js into one app.js file

--- gulp-basics/gulpfile.js
+++ gulp-basics/gulpfile.js
@@ -1,16 +1,19 @@
 'use strict';
 
 const gulp = require('gulp');
+const concat = require('gulp-concat');
 
-gulp.task('hello', (done) => {
-  console.log('Hello from gulp task!');
+gulp.task('concatScripts', (done) => {
+  // lists files in terh order they will appear
+  gulp.src([
+    'js/jquery.js',
+    'js/sticky/jquery.sticky.js',
+    'js/main.js'])
+    //pipes the above to the gulp-concat which ouputs app.js
+    .pipe(concat('app.js'))
+    // pipes the app.js file to the dest which specifies the dest folder
+    .pipe(gulp.dest('js'));
   done();
 });
 
-gulp.task('hello2', (done) => {
-  console.log('Hello from gulp task2!');
-  done();
-});
-
-
-gulp.task('default', gulp.series('hello', 'hello2'));
+gulp.task('default', gulp.series('concatScripts'));
